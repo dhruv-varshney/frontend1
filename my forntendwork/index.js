c339d253909b4605389f16ea1587aef77be5890d
dhruv-varshney <53403223+dhruv-varshney@users.noreply.github.com>
2020-10-02 22:56:13 +0530

    Update index.js

--- my forntendwork/index.js
+++ my forntendwork/index.js
@@ -4,10 +4,7 @@ var imgGreen = null;
 var imgBlue = null;
 var imgGrey = null;
 var imgRbow = null;
-var imgBlur = null;
-var imgWin = null;
 var imgReset = null;
-
 var file = document.getElementById("file");
 var canvas = null;
 
@@ -17,11 +14,9 @@ function load() {
   imgBlue = new SimpleImage(file);
   imgGreen = new SimpleImage(file);
   imgRbow = new SimpleImage(file);
-  imgBlur = new SimpleImage(file);
   imgReset = new SimpleImage(file);
   imgGrey = new SimpleImage(file);
-  imgWin = new SimpleImage(file);
-  canvas = document.getElementById("can");
+ canvas = document.getElementById("can");
   img.drawTo(canvas);
 }
 
@@ -97,84 +92,6 @@ function grey() {
   var canvas = document.getElementById("can");
   imgGrey.drawTo(canvas);
 }
-
-function getnewPixel(x, y) {
-  var h = imgBlur.getHeight();
-  var w = imgBlur.getWidth();
-  var newX = x + Math.round(Math.random() * 10) - 5;
-  var newY = y + Math.round(Math.random() * 10) - 5;
-
-  if (newX > w - 1) {
-    newX = w - 1;
-  } else if (newX < 0) {
-    newX = 0;
-  }
-
-  if (newY > h - 1) {
-    newY = h - 1;
-  } else if (newY < 0) {
-    newY = 0;
-  }
-
-  return (imgBlur.getPixel(newX, newY));
-}
-
-function blurImage() {
-  for (var pixel of imgBlur.values()) {
-    var random = Math.random();
-    var x = pixel.getX();
-    var y = pixel.getY();
-    if (random < 0.5) {
-      imgBlur.setPixel(x, y, pixel);
-    } else {
-      var newPixel = getnewPixel(x, y);
-      imgBlur.setPixel(x, y, newPixel);
-    }
-  }
-  var canvas = document.getElementById("can");
-  imgBlur.drawTo(canvas);
-}
-
-function setBorder(pixel) {
-  var pixelBorder = pixel.setRed(225);
-  pixelBorder = pixel.setGreen(255);
-  pixelBorder = pixel.setBlue(0);
-  return pixelBorder;
-}
-
-function windowFilter() {
-  var w = imgWin.getWidth();
-  var h = imgWin.getHeight();
-  var th = w * 0.0125;
-  var xx = (w - 4 * th) / 3;
-  var yy = (h - 3 * th) / 2;
-  for (var pixel of imgWin.values()) {
-    if (pixel.getY() < th || pixel.getY() >= h - th) {
-      setBorder(pixel);
-    }
-    if (pixel.getX() < th || pixel.getX() >= w - th) {
-      setBorder(pixel);
-    }
-    if (pixel.getX() > xx && pixel.getX() < xx + th) {
-      setBorder(pixel);
-    }
-    if (pixel.getX() > 2 * xx + th && pixel.getX() < 2 * xx + 2 * th) {
-      setBorder(pixel);
-    }
-    if (pixel.getY() > yy && pixel.getY() < yy + th) {
-      setBorder(pixel);
-    }
-  }
-  imgWin.drawTo(canvas);
-}
-
-function clearImage() {
-  var imgTemp = document.getElementById("can");
-  var clearImg = imgTemp.getContext("2d");
-  clearImg.clearRect(0, 0, imgTemp.width, imgTemp.height);
-  return;
-}
-
 function resetImage() {
   load();
 }
